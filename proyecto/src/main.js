b7bdb315063393ac7c257d9ccb155f5f2abfed60
samuelRB17 <samuel.ramirezb@uniagustiniana.edu.co>
2025-11-24 14:37:52 +0000

    creacion de pestañas home , login,logout,Original

--- proyecto/src/main.js
+++ proyecto/src/main.js
@@ -1,2 +1,41 @@
 import mostrarRegistro from './componentes/registro.js';
-mostrarRegistro()
+import mostrarLogin from './componentes/login.js';
+import mostrarOriginal from './componentes/Original.js';
+import mostrarHome from './componentes/Home.js';
+import mostrarLogout from './componentes/Logout.js';
+
+import { auth } from './firebaseConfig.js';
+import { onAuthStateChanged } from 'firebase/auth';
+onAuthStateChanged(auth, (user) => {
+if (user) {
+document.getElementById("menu").innerHTML = `
+<nav>
+<button id="menuHome">Home</button>
+<button id="menuOriginal">Original</button>
+<button id="menuLogout">Logout</button>
+</nav>
+`;
+document.getElementById("menuHome").addEventListener("click",
+mostrarHome);
+document.getElementById("menuOriginal").addEventListener("click",
+mostrarOriginal);
+document.getElementById("menuLogout").addEventListener("click",
+mostrarLogout);
+mostrarHome()
+} else{
+document.getElementById("menu").innerHTML = `
+<nav>
+
+<button id="menuLogin">Login</button>
+<button id="menuRegistro">Registro</button>
+</nav>
+`;
+document.getElementById("menuLogin").addEventListener("click",
+mostrarLogin);
+document.getElementById("menuRegistro").addEventListener("click",
+mostrarRegistro);
+mostrarLogin();
+}
+})
+
+
